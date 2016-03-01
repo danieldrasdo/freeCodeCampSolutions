6697e9e67b3866dd7b899bd08855925d4818cd10
Daniel Drasdo <danieldrasdo@gmail.com>
2016-03-01 11:55:57 -0800

    Updated where art thou
    My solution wasn't passing, so I had to redo it.

--- intermediate-algorithm-scripting/where-art-thou.js
+++ intermediate-algorithm-scripting/where-art-thou.js
@@ -1,29 +1,23 @@
 function where(collection, source) {
-  var arr = [];
+
+  var sourceKeys = Object.keys(source);
+
+  //Loop through the collection
   for (var i = 0; i < collection.length; i++) {
-    if (collection[i].hasOwnProperty(Object.keys(source))) {
-        var temp = Object.keys(source);
-        if (collection[i][temp]===source[temp]){
-        arr.push(collection[i]);
+
+    var currentObject = collection[i];
+
+    //Loop through all the sources keys
+    for (var j = 0; j < sourceKeys.length; j++) {
+      var currentKey = sourceKeys[j];
+
+      if ( !currentObject.hasOwnProperty(currentKey) || currentObject[currentKey] !== source[currentKey] ) {
+        collection.splice(i, 1);
+        i--;
       }
     }
   }
-  return arr;
+  return collection;
 }
 
-where(
-  [
-    {
-      first: 'Romeo',
-      last: 'Montague'
-    },
-    {
-      first: 'Mercutio',
-      last: null
-    },
-    {
-      first: 'Tybalt',
-      last: 'Capulet'
-    }
-  ],
-  {last: 'Capulet'});
+where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
